08d1b960645fb327c39d21bcfb6ee6b087bab99d
urgrod <lecam.gurvan@gmail.com>
2018-03-30 22:48:46 +0200

    hydrate

--- class/cambrure.php
+++ class/cambrure.php
@@ -104,5 +104,16 @@ class Cambrure{
     $this->$_fic_csv = $fic_csv;
   }
 
+
+  public function hydrate(array $data){
+    foreach($data as $key => $value){
+      $method ='set'.ucfirst($key);
+
+      if(method_exist($this, $method)){
+        $this->$method($value);
+        )
+      }
+    }
+  }
 }
- ?>
+?>
