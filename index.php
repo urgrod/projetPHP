9e5bd66157c5a0b27f8defe229c5bd5d5f21042b
azeaza <aexandre.moigne@hotmail.fr>
2018-04-04 16:10:02 +0200

    Création des fichiers de base

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Cambrure d'un profil Naca</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+    <script src="main.js"></script>
+</head>
+<body>
+<div class="container" id="header"> 
+        <div class="jumbotron">
+          <h1>Cambrure d'un profil Naca</h1>
+          <p>Bienvenue sur notre site</p>
+          <p>Il a été réalisé dans le cadre d'un projet dont le but était de representer un profil Naca</p>
+    </div>
+<div class="container" id="graph">
+
+</div>
+<div class="container" id="spreadsheet">
+
+</div>
+    
+</body>
+</html>
