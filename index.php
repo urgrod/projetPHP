b616547f51d50650f651eed03a0f1eacd1ec467c
azeaza <aexandre.moigne@hotmail.fr>
2018-04-04 16:23:30 +0200

    css

--- index.php
+++ index.php
@@ -5,24 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Cambrure d'un profil Naca</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
     <script src="main.js"></script>
 </head>
 <body>
-<div class="container" id="header"> 
+
+    <div class="container" id="header"> 
         <div class="jumbotron">
-          <h1>Cambrure d'un profil Naca</h1>
-          <p>Bienvenue sur notre site</p>
-          <p>Il a été réalisé dans le cadre d'un projet dont le but était de representer un profil Naca</p>
+            <h1>Cambrure d'un profil Naca</h1>
+            <p>Bienvenue sur notre site</p>
+            <p>Il a été réalisé dans le cadre d'un projet dont le but était de representer un profil Naca</p>
+        </div>
     </div>
-<div class="container" id="graph">
+    <div class="container" id="graph">
 
-</div>
-<div class="container" id="spreadsheet">
+    </div>
+    <div class="container" id="spreadsheet">
 
-</div>
-    
+    </div>
+        
 </body>
 </html>
