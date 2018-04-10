9d811b7a5031c033f1d37b06fbb1a9ffc2b15622
urgrod <lecam.gurvan@gmail.com>
2018-04-10 16:22:11 +0200

    calcul
    Co-Authored-By: azeaza <azeaza@users.noreply.github.com>

---
+++ class/calcul.php
@@ -0,0 +1,41 @@
+<?php
+
+
+/**
+*
+*/
+class Calcul
+{
+
+  public function calculX(){
+
+  }
+
+  public function calculT(){
+
+  }
+
+  public function calculF(){
+
+  }
+
+  public function calculYintra(){
+
+  }
+
+  public function calculYextra(){
+
+  }
+
+  public function calculLgx(){
+
+  }
+
+  public function calculG(){
+    
+  }
+
+}
+
+
+?>
---
+++ class/display.php
@@ -0,0 +1,27 @@
+<?php
+
+
+/**
+*
+*/
+class display
+{
+
+  public function displayOnePhoto(){
+
+    if(isset($_GET["id_photo"])){
+
+    }
+  }
+
+
+  public function displayAllPhoto(){
+
+      //appel de parametre->getList()
+      //affichage de toutes les photos avec leur libelle dans un for
+  }
+}
+
+
+
+?>
