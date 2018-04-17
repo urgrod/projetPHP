1f7c8be24d3fdfcd0244330e4ba939f0330229d1
azeaza <aexandre.moigne@hotmail.fr>
2018-04-17 16:26:09 +0200

    boostrap

--- include/includes.php
+++ include/includes.php
@@ -1,3 +1,10 @@
 <link rel="stylesheet" type="text/css" media="screen" href="/css/main.css" />
-<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+
+<link rel="stylesheet" type="text/css" href="/css/bootstrap-grid.css">
+<link rel="stylesheet" type="text/css" href="/css/bootstrap-grid.min.css">
+<link rel="stylesheet" type="text/css" href="/css/bootstrap-reboot.css">
+<link rel="stylesheet" type="text/css" href="/css/bootstrap-reboot.min.css">
 <link rel="stylesheet" type="text/css" href="/css/bootstrap-theme.min.css">
+<link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
